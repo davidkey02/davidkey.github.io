477a04e3e85e1c1184227e06c843df2ce4c1b5fd
davidkey02 <david.key8800@gmail.com>
2025-07-14 22:30:32 -0400

    gradebook
    added gradebook

---
+++ gradebook.js
@@ -0,0 +1,10 @@
+function fetchGradeData() {
+  console.log("fetchGradeData() called - will fetch grade data later.");
+}
+
+function populateGradebook() {
+  console.log("populateGradebook() called - will populate table later.");
+}
+
+fetchGradeData();
+populateGradebook();
