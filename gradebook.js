235a1bed25e99f4acd19764e274106130b3e18ae
davidkey02 <david.key8800@gmail.com>
2025-07-25 10:48:33 -0400

    Updated gradebook for fullstack

--- gradebook.js
+++ gradebook.js
@@ -1,10 +1,51 @@
 function fetchGradeData() {
-  console.log("fetchGradeData() called - will fetch grade data later.");
+    console.log("Fetching grade data...");
+
+    let xhr = new XMLHttpRequest();
+    let apiRoute = "/api/grades";
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                const results = JSON.parse(xhr.responseText);
+                populateGradebook(results);
+            } else {
+                console.error(`Could not get grades.\nStatus: ${xhr.status}`);
+            }
+        }
+    };
+
+    xhr.open("GET", apiRoute, true);
+    xhr.send();
 }
 
-function populateGradebook() {
-  console.log("populateGradebook() called - will populate table later.");
+function populateGradebook(data) {
+    console.log("Populating gradebook with data:", data);
+    let tableElm = document.getElementById("gradebook"); // Full table element
+
+    data.forEach(function (assignment) {
+        let row = document.createElement("tr");
+
+        // First TD: full name (last, first)
+        let nameCell = document.createElement("td");
+        nameCell.appendChild(
+            document.createTextNode(`${assignment.last_name}, ${assignment.first_name}`)
+        );
+
+        // Second TD: total grade
+        let gradeCell = document.createElement("td");
+        gradeCell.appendChild(
+            document.createTextNode(assignment.total_grade)
+        );
+
+        // Add to row
+        row.appendChild(nameCell);
+        row.appendChild(gradeCell);
+
+        // Add to table
+        tableElm.appendChild(row);
+    });
 }
 
+// Only call fetch here
 fetchGradeData();
-populateGradebook();
